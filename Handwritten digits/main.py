6927a84e1ca2181bb9e8e58cf0c705b3db985f00
Ben-Edwards44 <94799394+Ben-Edwards44@users.noreply.github.com>
2022-09-21 17:53:52 +0100

    Update main.py

--- Handwritten digits/main.py
+++ Handwritten digits/main.py
@@ -9,18 +9,18 @@ from time import sleep
 
 
 pygame.init()
-window = pygame.display.set_mode((500, 300))
-pygame.draw.line(window, (255, 255, 255), (304, 0), (304, 300), 4)
+window = pygame.display.set_mode((450, 250))
+pygame.draw.line(window, (255, 255, 255), (254, 0), (254, 250), 4)
 
 
 def check_click():
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
     if pygame.mouse.get_pressed(3)[0]:
-        if 0 <= mouse_x <= 294 and 0 <= mouse_y <= 300:
+        if 0 <= mouse_x <= 244 and 0 <= mouse_y <= 250:
             pygame.draw.circle(window, (255, 255, 255), (mouse_x, mouse_y), 4)
     elif pygame.mouse.get_pressed(3)[2]:
-        if 0 <= mouse_x <= 294 and 0 <= mouse_y <= 300:
+        if 0 <= mouse_x <= 244 and 0 <= mouse_y <= 250:
             pygame.draw.circle(window, (0, 0, 0), (mouse_x, mouse_y), 10)
 
     pygame.display.update()
@@ -34,7 +34,7 @@ def get_pixels():
     pixels = []
     for i, x in enumerate(pixel_array):
         pixels.append([])
-        for y in x[:300]:
+        for y in x[:250]:
             if y == 0:
                 pixels[i].append(0)
             else:
@@ -57,7 +57,7 @@ def load_network(filename):
 def pass_to_network():
     pixel_values = get_pixels()
     inputs = pixel_values
-    network = load_network("network.pickle")
+    network = load_network("better_network.pickle")
     
     display_results(network.calculate_output(inputs))
 
@@ -69,14 +69,14 @@ def display_results(outputs):
 
     new_outputs = dict(sorted(new_outputs.items(), key=lambda x:x[0], reverse=True))
 
-    pygame.draw.rect(window, (0, 0, 0), (308, 0, 200, 300))
-    font = pygame.font.Font('freesansbold.ttf', 20)
+    pygame.draw.rect(window, (0, 0, 0), (258, 0, 200, 300))
+    font = pygame.font.Font('freesansbold.ttf', 18)
 
     i = 1
     for output, value in new_outputs.items():
         text = font.render(f"{value}: {output * 100 :.2f}%", True, (255, 255, 255))
         txt_rect = text.get_rect()
-        txt_rect.center = (400, int(300 / 10 * i) - 15)
+        txt_rect.center = (350, int(250 / 10 * i) - 15)
         window.blit(text, txt_rect)
         i += 1
 
@@ -92,7 +92,7 @@ while True:
         pass_to_network()
         sleep(0.25)
     elif keys[pygame.K_c]:
-        pygame.draw.rect(window, (0, 0, 0), (0, 0, 300, 300))
+        pygame.draw.rect(window, (0, 0, 0), (0, 0, 250, 250))
         pygame.display.update()
 
     for e in pygame.event.get():
